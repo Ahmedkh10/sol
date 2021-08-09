f76192152c990d42d0a6073f6d5aea0165176162
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-09 11:02:43 +0100

    Update clients.php

--- clients.php
+++ clients.php
@@ -72,7 +72,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 						</div>
 					</div>	
 					<div class="col-sm-6 col-md-3">  
-						<a href="#" class="btn btn-success btn-block"> SSSSSss </a>  
+						<a href="#" class="btn btn-success btn-block"> SEA </a>  
 					</div>     
 				</div>
 				<!-- Search Filter -->
